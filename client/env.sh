50c821c07818549e41ab83333c192abcdfa1d3ba
Paul <parmati94@gmail.com>
2024-04-24 22:23:18 -0400

    Additional fixes for local deployment

--- client/env.sh
+++ client/env.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Provide a default value if the environment variable is empty
+REACT_APP_API_BASE_URL=${REACT_APP_API_BASE_URL:-'http://localhost:8000'}
+
 # Replace placeholders in env-config.js with environment variables
 sed -i 's|$REACT_APP_API_BASE_URL|'"$REACT_APP_API_BASE_URL"'|' /usr/src/app/build/env-config.js
